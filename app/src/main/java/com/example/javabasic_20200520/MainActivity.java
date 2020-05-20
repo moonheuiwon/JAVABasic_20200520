25a1fca664eb230c8cc8e560905ed18a81a19512
moonheuiwon <wonheuimoon@gmail.com>
2020-05-20 16:10:28 +0900

    멤버 변수의 연결

--- app/src/main/java/com/example/javabasic_20200520/MainActivity.java
+++ app/src/main/java/com/example/javabasic_20200520/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.javabasic_20200520;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText contentEdt;
+    Button okBtn;
+    TextView contentTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        contentEdt = findViewById(R.id.contentEdt);
+        okBtn = findViewById(R.id.okBtn);
+        contentTxt = findViewById(R.id.contentTxt);
     }
 }
