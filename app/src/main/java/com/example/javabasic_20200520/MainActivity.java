5b8b85adf009b98fd8a8116d35ba2df9b1c2a07a
moonheuiwon <wonheuimoon@gmail.com>
2020-05-20 16:59:45 +0900

    버튼 클릭 자체 이벤트 클릭 기초 작업

--- app/src/main/java/com/example/javabasic_20200520/MainActivity.java
+++ app/src/main/java/com/example/javabasic_20200520/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 
 import com.example.javabasic_20200520.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ActivityMainBinding binding;
 
@@ -21,11 +21,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        binding.okBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("메인화면","확인버튼 클릭");
-            }
-        });
+//        binding.okBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d("메인화면","확인버튼 클릭");
+//            }
+//        });
+
+        binding.okBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d("메인화면","자체 이벤트 코드");
     }
 }
